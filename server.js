edbb6f3a279c6f017c72f2f6cd067b520bca9cce
DaniIyar <94691892+DaniIyar@users.noreply.github.com>
2022-05-12 15:50:21 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = 27017;
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use(express.static('public'))
 
 app.set('view engine', 'ejs');
@@ -11,7 +14,27 @@ app.use("/", require("./routes/main"))
 app.use("/registration", require("./routes/registration"))
 app.use("/login", require("./routes/login"))
 app.use("/about", require("./routes/about"))
+app.use("/book", require("./routes/book"))
 app.use("/index", require("./routes/index1"))
+
+const userRoute = require('./routes/user')
+app.use('/user', userRoute)
+
+const dbConfig = require('./config/db.config');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("db connected successfully!");
+}).catch(err => {
+    console.log("couldn't connect to db", err)
+    process.exit();
+});
+
+app.get('/', (req, res) =>{
+    res.json({"message": "Hello Crud Node Express"})
+})
 
 app.listen(port, () =>
     console.log(`App listening at http://localhost:${port}`)
